f15f7ac6cbb1ad9de76e36df2cad02844dec53e6
amy <ming2tofu33@gmail.com>
2025-11-28 11:44:41 +0900

    Title: 함수를 이용한 369 게임, Time: 114ms, Memory: 17MB, Status: Passed - Codetree

--- 251128/함수를 이용한 369 게임/369-games-using-functions.py
+++ 251128/함수를 이용한 369 게임/369-games-using-functions.py
@@ -2,16 +2,19 @@ a, b = map(int, input().split())
 
 # Please write your code here.
 
-def game369(x, y):
-    cnt = 0
-
-    for i in range(x, y + 1):
-        tmp = list(str(i))
-        if i % 3 == 0:
-            cnt += 1
-        elif '3' in tmp or '6' in tmp or '9' in tmp:
-            cnt += 1
+def contains_369(n):
+    tmp = list(str(n))
+    if '3' in tmp or '6' in tmp or '9' in tmp:
+        return True
+    else:
+        return False
         
-    return cnt
+def is_369_num(n):
+    return contains_369(n) or (n % 3 == 0)
 
-print(game369(a, b))
+cnt = 0
+for i in range(a, b + 1):
+    if is_369_num(i):
+        cnt += 1
+
+print(cnt)
