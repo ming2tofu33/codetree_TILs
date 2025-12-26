0ffdc997ac21460a685700bef47020fe1d304764
amy <ming2tofu33@gmail.com>
2025-12-27 02:17:33 +0900

    Title: 코드네임, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251227/코드네임/code-name.py
+++ 251227/코드네임/code-name.py
@@ -1,14 +1,5 @@
 MAX_N = 5
 
-codenames = []
-scores = []
-for _ in range(MAX_N):
-    codename, score = input().split()
-    codenames.append(codename)
-    scores.append(int(score))
-
-# Please write your code here.
-
 class Test:
     def __init__(self, code, score):
         self.code = code
@@ -18,8 +9,9 @@ class Test:
         return f'{self.code} {self.score}'
 
 test_scores = []
-for i in range(MAX_N):
-    test_scores.append(Test(codenames[i], scores[i]))
+for _ in range(MAX_N):
+    codename, score = input().split()
+    test_scores.append(Test(codename, int(score)))
 
 test_scores.sort(key=lambda x: x.score)
 
