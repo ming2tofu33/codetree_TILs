128e16db7c3290fcdc9f71d0500a4870db872515
amy <ming2tofu33@gmail.com>
2026-01-01 01:02:58 +0900

    Title: 원점으로부터의 거리, Time: 66ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260101/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,9 @@
+n = int(input())
+points = [(int(i), tuple(map(int, input().split()))) for i in range(n)]
+
+# Please write your code here.
+
+points.sort(key=lambda x: (abs(x[1][0]) + abs(x[1][1]), x[0]))
+
+for point in points:
+    print(point[0] + 1)
