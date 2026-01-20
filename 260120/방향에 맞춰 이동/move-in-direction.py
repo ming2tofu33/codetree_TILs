bd16145707ff517de5b3e4b7e492eb4bf02b5029
amy <ming2tofu33@gmail.com>
2026-01-20 22:30:13 +0900

    Title: 방향에 맞춰 이동, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260120/방향에 맞춰 이동/move-in-direction.py
+++ 260120/방향에 맞춰 이동/move-in-direction.py
@@ -4,19 +4,20 @@ c = {'N': (0, 1), 'S': (0, -1), 'E': (1, 0), 'W': (-1, 0)}
 
 # Please write your code here.
 x, y = 0, 0
-print(moves)
-# for dir, dist in moves:
-#     if dir == 'N':
-#         x += c['N'][0] * dist 
-#         y += c['N'][1] * dist 
-#     elif dir == 'S':
-#         x += c['S'][0] * dist 
-#         y += c['S'][1] * dist 
-#     elif dir == 'E':
-#         x += c['E'][0] * dist 
-#         y += c['E'][1] * dist 
-#     elif dir == 'W':
-#         x += c['W'][0] * dist 
-#         y += c['W'][1] * dist 
 
-# print(x, y)    
+for dir, dist in moves:
+    dist = int(dist)
+    if dir == 'N':
+        x += c['N'][0] * dist 
+        y += c['N'][1] * dist 
+    elif dir == 'S':
+        x += c['S'][0] * dist 
+        y += c['S'][1] * dist 
+    elif dir == 'E':
+        x += c['E'][0] * dist 
+        y += c['E'][1] * dist 
+    elif dir == 'W':
+        x += c['W'][0] * dist 
+        y += c['W'][1] * dist 
+
+print(x, y)    
