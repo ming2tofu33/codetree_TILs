28fbfedc141a17e996a7d2388744815c5448e23d
amy <ming2tofu33@gmail.com>
2026-01-27 09:43:28 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260127/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,27 @@
+N, M = map(int, input().split())
+mat = [[0] * M for _ in range(N)]
+
+# Please write your code here.
+#    하, 우, 상, 좌
+dr = [1, 0, -1, 0]
+dc = [0, 1, 0, -1]
+r, c, d = 0, 0, 0
+
+def is_end(r, c):
+    return 0 > r or N <= r or 0 > c or M <= c
+        
+for num in range(1, N*M + 1):
+    mat[r][c] = num
+    nr = r + dr[d]
+    nc = c + dc[d]
+    if is_end(nr, nc) or mat[nr][nc] != 0:
+        d = (d + 1) % 4
+        nr = r + dr[d]
+        nc = c + dc[d]
+    r = nr
+    c = nc
+
+for i in range(N):
+    for j in range(M):
+        print(mat[i][j], end=' ')
+    print()
